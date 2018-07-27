8bbd416bebab54522430fa50e335e13177c33c01
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2018-07-27 16:25:30 +0200

    Add mix project

---
+++ backend/lib/backend.ex
@@ -0,0 +1,18 @@
+defmodule Canasta do
+  @moduledoc """
+  Documentation for Canasta.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Canasta.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ backend/test/backend_test.exs
@@ -0,0 +1,8 @@
+defmodule CanastaTest do
+  use ExUnit.Case
+  doctest Canasta
+
+  test "greets the world" do
+    assert Canasta.hello() == :world
+  end
+end
