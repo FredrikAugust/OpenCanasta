b32d02f400febd54ae02fe9f6865efd260b4774b
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2018-07-28 18:13:36 +0200

    Finish validation of melds, and canasta? function

--- backend/lib/backend/meld.ex
+++ backend/lib/backend/meld.ex
@@ -10,41 +10,82 @@ defmodule Canasta.Meld do
   @doc """
   Create a new meld from an array of cards. Optional parameter is minimum required (see Canasta.Meld.validate_min/2).
   """
-  def create_meld(cards, min \\ 0, final_round \\ false) do
-    
+  def create_meld(cards, score \\ 0, final_round \\ false) do
+    validation_results = [
+      validate_min(cards, score),
+      validate_number_of_cards(cards),
+      validate_purity(cards),
+      validate_ratio(cards, final_round)
+    ]
+
+    errors = validation_results 
+             |> Enum.reject(&(&1 == :ok))
+
+    if length(errors) == 0 do
+      %Canasta.Meld{cards: cards}
+    else
+      {:error, Enum.map(errors, &(elem(&1, 1)))}
+    end
   end
 
   @doc """
-  Checks if the cards provided are enough to surpass the minimum required. See table under for required value.
-
-  <0        = 15
-  0-1495    = 50
-  1500-2995 = 90
-  >3000     = 120
+  Returns the type of canasta (:natural, :dirty), or :no_canasta.
   """
-  defp validate_min(cards, min) do
+  def canasta?(meld) do
+    natural_cards = Enum.filter(Map.get(meld, :cards), &(Canasta.Card.card_type(&1) == :natural))
+    wild_cards = Enum.filter(Map.get(meld, :cards), &(Canasta.Card.card_type(&1) == :wild))
+
+    cond do
+      natural_cards >= 7 and wild_cards == 0 ->
+        :natural
+      natural_cards + wild_cards >= 7 ->
+        :dirty
+      true ->
+        :no_canasta
+    end
+  end
+
+  defp validate_min(cards, score) do# {{{
+  # Table of required min based on score
+  # <0        = 15
+  # 0-1495    = 50
+  # 1500-2995 = 90
+  # >3000     = 120
+
+    min = cond do
+      score in -10000..-5 ->
+        15
+      score in 0..1495 ->
+        50
+      score in 1500..2995 ->
+        90
+      true ->
+        120
+    end
+
     if Enum.reduce(Enum.map(cards, &Canasta.Card.value/1), &+/2) < min do
       {:error, "The value of the meld is lower than the minimum (#{min})."}
     else
       :ok
     end
-  end
+  end# }}}
 
-  @doc """
-  Checks if the meld has enough cards (3).
-  """
-  defp validate_number_of_cards(cards) do
+  defp validate_number_of_cards(cards) do# {{{
     if length(cards) < 3, do: {:error, "You need at least 3 cards in a meld."}, else: :ok
-  end
+  end# }}}
 
-  @doc """
-  Checks the ratio between natural cards and wild cards is correct. No more than 1:1, and at least 2 natural cards.
-  """
-  defp validate_ratio(cards, final_round) do
+  defp validate_ratio(cards, final_round) do# {{{
     if Enum.count(cards, &(Canasta.Card.card_type(&1, final_round) == :natural)) > length(cards)/2 do
       :ok
     else
       {:error, "You must have more natural cards than wild cards."}
     end
+  end# }}}
+
+  defp validate_purity(cards) do# {{{
+    num_uniques = Enum.filter(cards, &(Canasta.Card.card_type(&1) == :natural))
+                  |> Enum.uniq
+                  |> length
+    if num_uniques == 1, do: :ok, else: {:error, "You can only have one type of natural card in a meld."}
+  end# }}}
   end
-end
